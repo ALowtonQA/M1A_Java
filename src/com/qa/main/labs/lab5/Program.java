3c8215ee95acb66be633a356e0be329a4408081a
ALowtonQA <alowton@qa.com>
2023-11-22 15:36:32 +0000

    AL: lab 5

---
+++ src/com/qa/main/labs/lab5/Program.java
@@ -0,0 +1,16 @@
+package com.qa.main.labs.lab5;
+
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Lab05 lab5 = new Lab05();
+	
+		lab5.grades();
+		
+		lab5.part2();
+	
+	}
+}
