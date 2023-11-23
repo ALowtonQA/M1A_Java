cafd6d75380d0bfbfb6c4805c7473f469396ee3a
ALowtonQA <alowton@qa.com>
2023-11-23 13:12:22 +0000

    AL: lab6

---
+++ src/com/qa/main/labs/lab6/Program.java
@@ -0,0 +1,13 @@
+package com.qa.main.labs.lab6;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Lab6 lab6 = new Lab6();
+
+		lab6.part1();
+		
+		lab6.account(100, 200, 0.05);
+	}
+}
